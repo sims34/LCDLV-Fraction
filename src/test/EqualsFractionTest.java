f365107bd7fe4b2be31ee5f43cb20ccee849030c
sbens <maxfoerster86@gmail.com>
2022-08-08 12:26:16 +0200

    test equals: même dénominateur

--- src/test/EqualsFractionTest.java
+++ src/test/EqualsFractionTest.java
@@ -7,7 +7,9 @@ import org.junit.jupiter.api.Test;
 class EqualsFractionTest {
 
     @Test
-    void unEgaleUn(){
+    void memeDenominateur(){
         Assertions.assertEquals(new Fraction(1), new Fraction(1));
+        Assertions.assertEquals(new Fraction(1,3), new Fraction(1,3));
+        Assertions.assertEquals(new Fraction(3,4), new Fraction(3,4));
     }
 }
