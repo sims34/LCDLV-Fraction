7a44914370b0dc24f2d1f6ca7fa4401e5ab9eecf
sbens <maxfoerster86@gmail.com>
2022-08-09 11:26:22 +0200

    Soustraction: même dénominateur

--- src/test/SoustractionTest.java
+++ src/test/SoustractionTest.java
@@ -40,4 +40,10 @@ class SoustractionTest {
         Fraction result = new Fraction(-1).soustraction(new Fraction(-1));
         Assertions.assertEquals(new Fraction(0), result);
     }
+
+    @Test
+    void memeDenominateur() {
+        Fraction result = new Fraction(5, 3).soustraction(new Fraction(2, 3));
+        Assertions.assertEquals(new Fraction(1), result);
+    }
 }
